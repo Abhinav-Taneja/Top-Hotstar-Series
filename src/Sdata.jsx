dbf49e7d17c138df5c69bf396082bd3dddf99ce7
Abhinav-Taneja <abhinavtaneja00@gmail.com>
2021-03-28 21:00:52 +0530

    Top Hotstar Series

---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+    const Sdata=[ 
+    {
+        imgsrc:"https://upload.wikimedia.org/wikipedia/en/b/bf/Criminal_Justice_India.jpeg",
+        sname: "Criminal Justice",
+        title: "A Hotstar Orignal Series",
+        link: "https://www.hotstar.com/in/tv/criminal-justice/1260004267"
+    },
+    {
+        imgsrc: "https://blogtobollywood.com/wp-content/uploads/2020/03/Special-OPS-download.jpg",
+  title: "A Hotstar Orignal Series",
+  sname: "Special OPS",
+  link: "https://www.hotstar.com/in/tv/special-ops/1260022894" 
+    },
+    {
+        imgsrc: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F0%2F02%2FHostages_Hotstar_Poster.jpg&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FHostages_(Indian_TV_series)&tbnid=Y9f3ymYCCcX62M&vet=12ahUKEwjDpPfjpdPvAhW3oUsFHZjIDM8QMygAegUIARCnAQ..i&docid=F1ZfVEzqgjyH1M&w=316&h=316&q=hostages%20web%20series%20photo&ved=2ahUKEwjDpPfjpdPvAhW3oUsFHZjIDM8QMygAegUIARCnAQ",
+        sname: "Hostages",
+        title:"A Hotstar Orignal Series",
+        link: "https://www.hotstar.com/in/tv/hostages/1260005113"  
+    },
+
+    {
+        imgsrc: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi2.wp.com%2Fwww.webseriesreviews.com%2Fwp-content%2Fuploads%2F2020%2F06%2FAarya-Web-Series-Review.jpg%3Ffit%3D696%252C343%26ssl%3D1&imgrefurl=https%3A%2F%2Fwww.webseriesreviews.com%2Faarya-review%2F&tbnid=uiMCNJNga5pB7M&vet=12ahUKEwjU-d-JptPvAhVuiksFHTQNAXIQMygAegUIARCjAQ..i&docid=a9OrqzMivdBPiM&w=696&h=343&q=aarya%20web%20series%20photos&ved=2ahUKEwjU-d-JptPvAhVuiksFHTQNAXIQMygAegUIARCjAQ",
+sname: "Aarya",
+title: "A Hotstar Orignal Series",
+link: "https://www.hotstar.com/in/tv/aarya/1260028094",
+    },
+
+    {
+        imgsrc: "https://www.google.com/imgres?imgurl=https%3A%2F%2Ftopleadindia.com%2Fwp-content%2Fuploads%2F2019%2F11%2Fout-of-love-thumb.jpg&imgrefurl=https%3A%2F%2Ftopleadindia.com%2F2019%2F11%2F03%2Fout-of-love-first-look-of-hotstars-new-web-series-on-infidelity-released%2F&tbnid=NJEcTXOUFBdOXM&vet=12ahUKEwjvzey6ptPvAhULcCsKHcRxDKIQMygAegUIARCdAQ..i&docid=31KFu-URCeu3EM&w=660&h=373&q=out%20of%20love%20web%20series%20photo&ved=2ahUKEwjvzey6ptPvAhULcCsKHcRxDKIQMygAegUIARCdAQ",
+sname: "Out of Love",
+title: "A Hotstar Orignal Series",
+link: "https://www.hotstar.com/in/tv/out-of-love/1260013972"
+    },
+
+];
+
+export default Sdata;
